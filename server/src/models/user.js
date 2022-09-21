e0e8b349d207b6cc84810f8c0534fb69ddff4467
ekgns33 <ekgns00421@gmail.com>
2022-09-21 23:01:22 +0900

    update: add save method which insert new user into database

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -5,6 +5,15 @@ async function getAllUsers() {
   return await knex('user').select('user_email');
 }
 
+async function findOne(column_name, value) {
+  const user = await knex('user').select('*').where(column_name, value);
+  return user;
+}
+
+async function save(body) {
+  //TODO: mysql이 insert한 객체를 리턴하는 함수를 지원하지 않음. success하면 어떻게 리턴할지고민해야함.
+  return await knex('user').insert(body);
+}
 async function findTargetUserByEmail(user_email) {
   console.log(user_email);
   const targetUser = await knex('user')
@@ -13,4 +22,4 @@ async function findTargetUserByEmail(user_email) {
 
   return targetUser;
 }
-module.exports = { getAllUsers, findTargetUserByEmail };
+module.exports = { getAllUsers, findTargetUserByEmail, findOne, save };
