db1a81cc2f662451d2881abf733f2c5572214b0b
suhwan2004 <suhwan2004@gmail.com>
2022-10-18 20:09:45 +0900

    feature : add Header and Routes in App.jsx

---
+++ Client/src/App.jsx
@@ -0,0 +1,23 @@
+import _config from './config';
+import { Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Home from './pages/Home/Home';
+import { Header } from './components';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
+
+function App() {
+    const theme = createTheme(_config.themes);
+    return (
+        <ThemeProvider theme={theme}>
+            <Header />
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/signIn" exact element={<SignIn />} />
+                <Route path="/eventPost" exact element={<SignUp />} />
+            </Routes>
+        </ThemeProvider>
+    );
+}
+
+export default App;
