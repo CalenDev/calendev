a9221b6e767505e18951f36c2d40169640621d62
suhwan2004 <suhwan2004@gmail.com>
2022-10-24 21:14:39 +0900

    feature : make basic FindPw page.(no apply style)

---
+++ Client/src/pages/FindPw/FindPw.jsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import { Alert } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+// import Paper from '@mui/material/Paper';
+// import { postFindPw } from '../../api';
+import { validateEmail } from '../../utils';
+
+/*
+진행 완료
+
+- 사용자는 비밀번호를 찾기 위하여 회원가입 할 시 사용하였던 이메일을 기입하여야 한다.
+  - 이메일 유효성 검사 실패 시에도 Alert message 출력
+- 사용자가 이메일을 입력 하였을 시, 해당 이메일을 가진 회원이 존재하지 않는다면 해당 이메일을 가진 회원이 없음을 사용자에게 알려 주어야 한다.
+  - 이메일 유효성 검사 실패 시와 마찬가지로 하단에 Alert message가 출력됨
+
+진행중
+- 사용자가 이메일을 입력 하였을 시, 해당 이메일을 가진 회원이 존재한다면 그 이메일로 해당 사용자의 비밀번호 초기화를 위한 링크를 보내주어야 한다
+  - server에서 처리
+*/
+function FindPw() {
+  const [alertMessageStatus, setAlertMessageStatus] = useState('');
+  const alertMessageObj = {
+    success: '비밀번호 초기화 링크를 이메일 주소로 전달했습니다.',
+    error: '존재하지 않는 이메일입니다. 다시 한번 확인해주세요.',
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const EmailTextField = e.target.querySelector('input');
+    const EmailValidationResult = validateEmail(EmailTextField.value);
+
+    // 1. validate email check
+    if (!EmailValidationResult) {
+      setAlertMessageStatus('error');
+      return;
+    }
+
+    // 2. request
+    /* const apiRes = await postFindPw(EmailTextField.value);
+    if (apiRes.status === 200) {
+      setAlertMessageStatus('success');
+    } else if (apiRes.status === 400) {
+      setAlertMessageStatus('error');
+    } */
+    setAlertMessageStatus('success');
+  };
+
+  return (
+    <StyledFindPwContainer component="main" maxWidth="md">
+      <Typography varient="h5">비밀번호 찾기</Typography>
+      <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+          margin="normal"
+          fullWidth
+          id="email"
+          label="이메일"
+          name="email"
+          autoFocus
+          inputProps={{
+            maxLength: 20,
+          }}
+        />
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          비밀번호 찾기
+        </Button>
+      </Box>
+      <FindPwAlert
+        alertMessageObj={alertMessageObj}
+        alertMessageStatus={alertMessageStatus}
+      />
+    </StyledFindPwContainer>
+  );
+}
+
+/*
+const CustomPaper = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: auto;
+  padding: ${(props) => props.theme.spacing(4, 2)};
+  margin: ${(props) => props.theme.spacing(12, 1, 'auto')};
+`; */
+
+function FindPwAlert(props) {
+  const { alertMessageStatus, alertMessageObj } = props;
+  const alertMessage = alertMessageObj[alertMessageStatus];
+
+  return (
+    <Alert
+      severity={alertMessage ? alertMessageStatus : 'success'}
+      variant="outlined"
+      iconMapping={{
+        success: <CheckCircleOutlineIcon fontSize="inherit" />,
+      }}
+      color={alertMessageStatus === 'success' ? 'primary' : 'error'}
+      sx={{
+        visibility: `${alertMessage ? 'visible' : 'hidden'}`,
+      }}
+    >
+      {alertMessageObj[alertMessageStatus]}
+    </Alert>
+  );
+}
+const StyledFindPwContainer = styled(Container)`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+export default FindPw;
