8a6b0be47404e46625ca9bfc6d0f7e4ac3cae678
suhwan2004 <suhwan2004@gmail.com>
2022-10-25 20:30:50 +0900

    setup : add basic project files

---
+++ Client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { config } from './config';
+import Home from './pages/Home/Home';
+
+function App() {
+  const theme = createTheme(config.themes);
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+export default App;
