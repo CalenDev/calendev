8b89270db3ec167c7e1185098dd4ebf3c4714ee7
suhwan2004 <suhwan2004@gmail.com>
2022-10-27 20:26:53 +0900

    feature : edit parameter in Reset Password fetch function

--- Client/src/api/auth.js
+++ Client/src/api/auth.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const putResetPw = async () => {
+const putResetPw = async (token, userPassword) => {
   const API_END_POINT = `${process.env.REACT_APP_SERVER_URL}/api/v1/auth/password`;
   try {
-    const { data } = await axios.put(API_END_POINT, {});
+    const { data } = await axios.put(API_END_POINT, {
+      token,
+      userPassword,
+    });
     return data;
   } catch (e) {
     return { status: e.response.status };
