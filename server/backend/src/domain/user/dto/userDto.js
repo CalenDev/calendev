153849bd260a9bc6b591e38d389cd05470160445
ekgns33 <ekgns00421@gmail.com>
2022-11-03 20:47:51 +0900

    create: create userDto file for userUpdateService

---
+++ server/backend/src/domain/user/dto/userDto.js
@@ -0,0 +1,26 @@
+class UserDto {
+  userEmail;
+  userNickname;
+  userRoleCd;
+
+  constructor() {}
+
+  get getUserEmail() {
+    return this.userEmail;
+  }
+  get getUserNickName() {
+    return this.userNickname;
+  }
+  get getUserRoleCd() {
+    return this.userRoleCd;
+  }
+  set setUserEmail(userEmail) {
+    this.userEmail = userEmail;
+  }
+  set setUserNickname(userNickName) {
+    this.userNickname = userNickName;
+  }
+  set setUserRoleCd(userRoleCd) {
+    this.userRoleCd = userRoleCd;
+  }
+}
