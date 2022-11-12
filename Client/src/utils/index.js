a7c793c81db1ad217dc4746bfc6a527cae8b03c0
suhwan2004 <suhwan2004@gmail.com>
2022-11-13 06:17:35 +0900

    feature : create utils/index.js
    Co-authored-by: dayeon-alert <dayeon.alert@gmail.com>

---
+++ Client/src/utils/index.js
@@ -0,0 +1,11 @@
+import { validateEmail, validatePhone, validatePlace } from './validationCheck';
+import commonMsgText from './commonMsgText';
+import dttmFormatter from './formatter';
+
+export {
+  validateEmail,
+  validatePhone,
+  validatePlace,
+  commonMsgText,
+  dttmFormatter,
+};
