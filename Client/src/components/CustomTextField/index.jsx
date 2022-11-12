dcdf5f72b197abb7936b35e7761df33147a29635
Dayeon <dayeon.alert@gmail.com>
2022-11-12 23:35:21 +0900

    DEV-14 CustomTextField 공통 컴포넌트 구현 (#17)

---
+++ Client/src/components/CustomTextField/index.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import TextField from '@mui/material';
+import commonMsgText from '../../utils/commonMsgText';
+
+export default function CustomTextField(props) {
+  const { helpermsgobj } = props;
+  const textHelperMsgObj = helpermsgobj === undefined ? { code: 0 } : helpermsgobj;
+
+  return (
+    <StyledTextField
+      size="small"
+      error={textHelperMsgObj.code % 10 !== 0}
+      helperText={commonMsgText(textHelperMsgObj.code, textHelperMsgObj.arg1)}
+      variant="outlined"
+      fullWidth
+      {...props}
+    />
+  );
+}
+
+CustomTextField.propTypes = {
+  helpermsgobj: PropTypes.shape({
+    code: PropTypes.number.isRequired,
+    arg1: PropTypes.string,
+  }),
+};
+
+CustomTextField.defaultProps = {
+  helpermsgobj: { code: 0 },
+};
+
+const StyledTextField = styled(TextField)`
+  & .MuiInputBase-input {
+    padding: ${(props) => props.theme.spacing(0.5, 2)};
+    font-size: 0.9rem;
+  }
+  & .MuiFormHelperText-root {
+    margin-left: 7px;
+    margin-right: 0;
+  }
+`;
