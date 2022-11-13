52a9c3fd671cdec55e1fd52ea394cb458fcc5a71
ekgns33 <ekgns00421@gmail.com>
2022-11-13 20:28:20 +0900

    DEV-52 [Settings] fix: add typecast for datetime

--- server/backend/src/global/config/knexConfig.js
+++ server/backend/src/global/config/knexConfig.js
@@ -1,5 +1,6 @@
 import knex from 'knex';
 import dotenv from 'dotenv';
+import moment from 'moment';
 
 dotenv.config({ path: './.env' });
 const instance = knex({
@@ -9,6 +10,13 @@ const instance = knex({
     user: process.env.LOCAL_DATABASE_USER, // 실제 서비스에서는 root 계정을 사용하지 않는 것이 좋습니다.
     password: process.env.LOCAL_DATABASE_PASSWORD,
     database: process.env.LOCAL_DATABASE_NAME,
+    timezone: 'UTC',
+    typeCast(field, next) {
+      if (field.type === 'DATETIME') {
+        return moment(field.string()).format('YYYY-MM-DD HH:mm:ss');
+      }
+      return next();
+    },
   },
 });
 
