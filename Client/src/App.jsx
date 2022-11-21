71b7a838c3a5255c91b7f9672d709b5a85077772
suhwan2004 <suhwan2004@gmail.com>
2022-11-21 22:28:25 +0900

    feature : router path를 다시 signin, home이 존재하도록 정상변경

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import themes from './styles/themes';
 import SignIn from './pages/SignIn/SignIn';
+import Home from './pages/Home/Home';
 
 function App() {
   const theme = createTheme(themes);
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" exact element={<SignIn />} />
+        <Route path="/signin" exact element={<SignIn />} />
+        <Route path="/" exact element={<Home />} />
       </Routes>
     </ThemeProvider>
   );
