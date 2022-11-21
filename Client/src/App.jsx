aecc9299253c5d01d44c64e2676da047fbf07f79
suhwan2004 <suhwan2004@gmail.com>
2022-11-21 21:42:35 +0900

    feature : path 임시수정

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import themes from './styles/themes';
-import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn/SignIn';
 
 function App() {
@@ -9,8 +8,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/signIn" exact element={<SignIn />} />
+        <Route path="/" exact element={<SignIn />} />
       </Routes>
     </ThemeProvider>
   );
