a25311e8860b38fef3023a5c341c5e39cd013f0f
suhwan2004 <suhwan2004@gmail.com>
2022-11-21 23:55:13 +0900

    feature : 전역에 설정된 withCredential 설정 해제

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
-import axios from 'axios';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.withCredentials = true;
 
 root.render(
   <React.StrictMode>
