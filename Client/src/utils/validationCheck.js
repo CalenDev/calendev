edf1cf2b1ac5e22795a67e02d4727b73fffe9b59
suhwan2004 <suhwan2004@gmail.com>
2022-11-22 00:35:17 +0900

    feature : validateCheck.js내에 존재하는 validate function의 네이밍을 validateRex...가 되도록 수정

--- Client/src/utils/validationCheck.js
+++ Client/src/utils/validationCheck.js
@@ -1,9 +1,9 @@
-export function validateEmail(email) {
+export function validateRegexEmail(email) {
   const reg = /^.+@.{2,}..{2,}$/;
   return reg.test(email);
 }
 
-export function validatePassword(password) {
+export function validateRegexPassword(password) {
   const reg =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
   return reg.test(password);
