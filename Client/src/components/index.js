3e24696c9e8cd5273bfc476398b546fb021bc3c7
suhwan2004 <suhwan2004@gmail.com>
2022-11-26 23:22:23 +0900

    feature : private route를 components/index.js에서 바로 export 받을 수 있도록 수정

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import CommonPaper from './CommonPaper';
 import CustomTextField from './CustomTextField';
+import PrivateRoute from './PrivateRoute';
 
-export { Header, CommonPaper, CustomTextField };
+export { Header, CommonPaper, CustomTextField, PrivateRoute };
