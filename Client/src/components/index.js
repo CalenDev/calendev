e57e9305baf7f28d90cb1ba87f1455e4aa45f202
suhwan2004 <suhwan2004@gmail.com>
2022-11-27 15:13:17 +0900

    feature : components/index.js에 현재 만든 CommonSearchBar 추가.

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import CommonPaper from './CommonPaper';
 import CustomTextField from './CustomTextField';
+import CommonSearchBar from './CommonSearchBar';
 
-export { Header, CommonPaper, CustomTextField };
+export { Header, CommonPaper, CustomTextField, CommonSearchBar };
