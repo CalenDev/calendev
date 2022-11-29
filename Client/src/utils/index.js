5f8b41ef56a57b180fa61fc6f6f082f45d9e9c71
suhwan2004 <suhwan2004@gmail.com>
2022-11-29 22:57:41 +0900

    feature : urlQueryParser util funcition 추가

--- Client/src/utils/index.js
+++ Client/src/utils/index.js
@@ -1,4 +1,10 @@
 import { validateRegexEmail, validateRegexPassword } from './validationCheck';
 import commonMsgText from './commonMsgText';
+import urlQueryParser from './url';
 
-export { validateRegexEmail, validateRegexPassword, commonMsgText };
+export {
+  validateRegexEmail,
+  validateRegexPassword,
+  commonMsgText,
+  urlQueryParser,
+};
