ac82a03d3c3967cd1233436719b781dc40019b72
ekgns33 <ekgns00421@gmail.com>
2022-12-02 00:21:45 +0900

    DEV-56 [Email Service] feature: 비밀번호 재설정 기능추가

--- server/backend/src/domain/user/routes/userRoutes.js
+++ server/backend/src/domain/user/routes/userRoutes.js
@@ -1,18 +1,26 @@
 import express from 'express';
-import authController from '../controllers/authController.js';
+import authControllers from '../controllers/authController.js';
 import userControllers from '../controllers/userController.js';
 
 const router = express.Router();
+
+router.route('/login').post(authControllers.userLogIn);
+
+router.route('/refresh').get(authControllers.refreshJWT);
+
+// 토큰정보를 기반으로 권한이 있는지 미들웨어에서 확인.
+router
+  .route('/signinOptions/forgotPassword')
+  .post(authControllers.sendResetEmail);
+
+router
+  .route('/signinOptions/resetPassword/:token')
+  .patch(authControllers.resetPassword);
 
 router.route('/').get(userControllers.getAllUsers);
 
 router.route('/check-duplicate').post(userControllers.checkDuplicate);
 
 router.route('/signup').post(userControllers.signupUser);
-
-// 토큰정보를 기반으로 권한이 있는지 미들웨어에서 확인.
-router
-  .route('/signinOptions/resetPassword')
-  .post(userControllers.sendResetEmail);
 
 export default router;
