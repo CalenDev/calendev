0c7486061a9d69f4caa524a864ee3c5ce7e1e554
suhwan2004 <suhwan2004@gmail.com>
2022-12-03 20:41:40 +0900

    feature : modal을 최 상위인 App.js에서 관리되도록 코드 적용

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,51 @@
-import { Route, Routes } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import { useState, useEffect } from 'react';
 import Home from './pages/Home/Home';
 import { Header } from './components';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import ResetPw from './pages/ResetPw/ResetPw';
 import themes from './styles';
+import CommonModal from './components/CommonModal/index';
 
 function App() {
-  const theme = createTheme(themes);
+  const theme = responsiveFontSizes(createTheme(themes));
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalInfo, setModalInfo] = useState({});
+
+  useEffect(() => {
+    if (location.state) {
+      const { modalTitle, modalDescription } = location.state;
+      if (modalTitle && modalDescription && !modalOpen) {
+        setModalOpen(true);
+        setModalInfo({ modalTitle, modalDescription });
+        navigate(location.pathname, { replace: true });
+      }
+    }
+  }, [location, modalOpen, modalInfo, navigate]);
+
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      {modalOpen ? (
+        <CommonModal
+          open={modalOpen}
+          onClose={() => {
+            setModalOpen(false);
+            setModalInfo({});
+          }}
+          modalInfo={modalInfo}
+        />
+      ) : (
+        <div />
+      )}
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/signin" exact element={<SignIn />} />
