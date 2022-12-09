979a12c0f7687bf3642600a1e707a5584fc4f0b5
suhwan2004 <suhwan2004@gmail.com>
2022-12-09 19:35:41 +0900

    feature : redux, redux-tookit 설치

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,56 +1,30 @@
-import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import {
   createTheme,
   responsiveFontSizes,
   ThemeProvider,
 } from '@mui/material/styles';
-import { useState, useEffect } from 'react';
 import Home from './pages/Home/Home';
 import { Header } from './components';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import ResetPw from './pages/ResetPw/ResetPw';
 import themes from './styles';
-import CommonModal from './components/CommonModal/index';
+import ModalContainer from './container/ModalContainer';
+import Error from './pages/Error/Error';
 
 function App() {
   const theme = responsiveFontSizes(createTheme(themes));
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [modalOpen, setModalOpen] = useState(false);
-  const [modalInfo, setModalInfo] = useState({});
-
-  useEffect(() => {
-    if (location.state) {
-      const { modalTitle, modalDescription } = location.state;
-      if (modalTitle && modalDescription && !modalOpen) {
-        setModalOpen(true);
-        setModalInfo({ modalTitle, modalDescription });
-        navigate(location.pathname, { replace: true });
-      }
-    }
-  }, [location, modalOpen, modalInfo, navigate]);
-
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      {modalOpen ? (
-        <CommonModal
-          open={modalOpen}
-          onClose={() => {
-            setModalOpen(false);
-            setModalInfo({});
-          }}
-          modalInfo={modalInfo}
-        />
-      ) : (
-        <div />
-      )}
+      <ModalContainer />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/signin" exact element={<SignIn />} />
         <Route path="/signup" exact element={<SignUp />} />
         <Route path="/resetpw" exact element={<ResetPw />} />
+        <Route path="/error" exact element={<Error />} />
       </Routes>
     </ThemeProvider>
   );
