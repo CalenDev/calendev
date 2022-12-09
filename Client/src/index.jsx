671de5a4b929f309197b997493321d9c0cc6acd5
suhwan2004 <suhwan2004@gmail.com>
2022-12-09 19:37:28 +0900

    feature : index.js에 store 적용

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import store from './slices';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <CssBaseline />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
 );
