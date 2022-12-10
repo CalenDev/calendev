3c764af3e1ee3d418219c8594014128e17d05fb7
suhwan2004 <suhwan2004@gmail.com>
2022-12-11 02:30:45 +0900

    feature : CommonModal 삭제에 따라, components/index.js에서 CommonModal Import 삭제

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,6 +1,5 @@
 import Header from './Header';
 import CommonPaper from './CommonPaper';
 import CustomTextField from './CustomTextField';
-import CommonModal from './CommonModal';
 
-export { Header, CommonPaper, CustomTextField, CommonModal };
+export { Header, CommonPaper, CustomTextField };
