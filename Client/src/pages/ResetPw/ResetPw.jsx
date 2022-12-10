8bf84d6c0234ec9dddcadeb52cb5253a8b322512
suhwan2004 <suhwan2004@gmail.com>
2022-12-11 02:32:21 +0900

    feature : ResetPw page에서 수정된 GlobalModal과의 dispatch event 적용

--- Client/src/pages/ResetPw/ResetPw.jsx
+++ Client/src/pages/ResetPw/ResetPw.jsx
@@ -1,26 +1,14 @@
 import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { useDispatch } from 'react-redux';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { useLocation, useNavigate } from 'react-router-dom';
-import styled from '@emotion/styled';
 import { validateRegexPassword, urlQueryParser } from '../../utils';
 import { putResetPw, getCheckResetPasswordToken } from '../../api';
 import { CommonPaper, CustomTextField } from '../../components';
-
-/*
-main logic
-
-1. 사용자의 경우 findPw 페이지에서 이메일로 resetPw 페이지의 링크를 받습니다.
-2. 이메일에서 resetPw 페이지로 접근합니다.
-3. 첫 렌더링 시에, 서버에 토큰값만 담고 1회 token의 유효기간 검증 요청을 실시합니다. => 토큰의 유효시간은 1시간입니다.
-  (1). 토큰이 만료됬다면, 토큰을 다시 받아달라는 메시지를 홈 페이지로 이동 이후 띄워줍니다. => 토큰의 기간만료는 서버에러가 아니기에 error 페이지로 이동하지 않음.
-  (2). 토큰이 만료되지 않았다면, resetPw페이지를 띄워줍니다.
-4. 유효성 겁사를 통과한 새 비밀번호 입력을 완료했다면 서버에 비밀번호 초기화 요청을 합니다. 이때, 토큰과 새 비밀번호를 서버로 보냅니다.
-  200번대 : 서버에서 정상적으로 비밀번호가 변경됬다는 판정이 나온다면 로그인 페이지로 이동합니다.
-  400번대 : 토큰의 유효기간이 만료되었다면 토큰을 다시 받아달라는 메시지를 홈 페이지로 이동 이후 띄워줍니다.
-  500번대 : 서버에러입니다. error 페이지로 보냅니다.
-  */
+import { openModal } from '../../features/GlobalModal/GlobalModalSlice';
 
 function ResetPw() {
   const navigate = useNavigate();
@@ -31,38 +19,43 @@ function ResetPw() {
   const [checkedPwMsgObj, setCheckedPwMsgObj] = useState({ code: 0, arg1: '' });
   const { search } = useLocation();
   const queryStringObj = urlQueryParser(search);
+  const dispatch = useDispatch();
+
+  const handleOpenModal = (modalCode) => {
+    dispatch(openModal({ modalCode }));
+  };
 
   // check token is expired
   useEffect(() => {
     async function checkTokenExpired() {
+      // 비 정상적 접근 : redirect to home
       if (!queryStringObj.token) {
         navigate('/', {
           replace: true,
-          state: {
-            modalTitle: '비정상적인 접근',
-            modalDescription: '정상적인 경로로 사이트를 이용해주세요.',
-          },
         });
+        handleOpenModal(1);
         return;
       }
 
       const apiRes = await getCheckResetPasswordToken(queryStringObj.token);
       switch (apiRes.status) {
-        case 200: // token is not expired
+        case 200: // status 200 : valid token. render resetPw page
           break;
-        case 401:
-          // token is expired
+        case 401: // status 401 : unvalid token. redirect to home.
+          navigate('/', {
+            replace: true,
+          });
+          handleOpenModal(2);
+          break;
+        case 500: // status 500 : server error. redirect to error page.
           navigate('/', {
             replace: true,
             state: {
-              modalTitle: '유효기간 만료',
-              modalDescription:
-                '유효기간이 만료되었습니다. 링크를 다시 받아주세요',
+              errorTitle: 'Calendev Server Error',
             },
           });
           break;
         default:
-          // server Error! go to error page
           break;
       }
     }
@@ -101,21 +94,22 @@ function ResetPw() {
 
     const apiRes = await putResetPw(queryStringObj.token, newPassword);
     switch (apiRes.status) {
-      case 200:
+      case 200: // status 200 : success to reset password
         navigate('/signin', { replace: true });
         break;
-      case 404:
+      case 401: // status 401 : token unvalid
+        navigate('/', {
+          replace: true,
+        });
+        handleOpenModal(2);
+        break;
+      case 500:
         navigate('/', {
           replace: true,
           state: {
-            modalTitle: '유효기간 만료',
-            modalDescription:
-              '유효기간이 만료되었습니다. 링크를 다시 받아주세요',
+            errorTitle: 'Calendev Server Error',
           },
         });
-        break;
-      case 500:
-        console.log('server error! go error page!');
         break;
       default:
         break;
