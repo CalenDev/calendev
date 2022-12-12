29ba0af4b2e68909d29288dcf099c9b473bf8430
suhwan2004 <suhwan2004@gmail.com>
2022-12-12 13:01:23 +0900

    feature : UserSlice를 persistStore에 추가

---
+++ Client/src/store/persistedReducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage/session';
+import User from '../feature/User/UserSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['User'],
+};
+
+const rootReducer = combineReducers({
+  User,
+});
+
+export default persistReducer(persistConfig, rootReducer);
