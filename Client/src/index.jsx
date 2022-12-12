16633b49e324577f7a4b96473a38df102066f82c
suhwan2004 <suhwan2004@gmail.com>
2022-12-12 13:01:48 +0900

    feature : index.js에 redux 적용

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -1,16 +1,25 @@
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App';
+import store, { persistor } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CssBaseline />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <CssBaseline />
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>,
 );
