f5f96c61e369a45161c7e4af70be760c826d1772
ekgns33 <ekgns00421@gmail.com>
2023-01-16 03:01:25 +0900

    DEV-54 fix: enum추가에 따른 에러 처리 방식 변경

--- server/backend/src/domain/user/controllers/errorController.js
+++ server/backend/src/domain/user/controllers/errorController.js
@@ -1,27 +1,6 @@
 /* eslint-disable no-param-reassign */
 import AppError from '../../../global/utils/appError.js';
 
-const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
-};
-// const handleDuplicateFieldDB = (err) => {
-//   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-//   console.log(value);
-// };
-// const handleDuplicateEntryDB = (err) => {
-//   const message = `Duplicate Entry exists!`;
-//   return new AppError(message, 401);
-// };
-const handleJsonWebTokenError = (err) => {
-  const message = `JsonWebTokenError : ${err.detail}`;
-  return new AppError(message, 400);
-};
-const handleNotBeforeError = (err) => {
-  const message = `NotBeforeError: ${err.message} | ${err.date}`;
-  return new AppError(message, 400);
-};
-
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -37,15 +16,10 @@ const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message,
       code: err.errorCode,
     });
   } else {
     // Programming error or unknown error : 클라이언트에게 미표시
-    // 1) log error
-    console.error('ERROR!! ❌', err);
-
-    // 2) Send generic message
     res.status(500).json({
       status: 'error',
       message: 'Something went wrong!',
@@ -56,7 +30,7 @@ const sendErrorProd = (err, res) => {
 export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
+  console.log(err.stack);
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
