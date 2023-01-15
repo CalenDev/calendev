23f95f83ae555c47bb589bd2c6c3b3b82fb8ef1c
ekgns33 <ekgns00421@gmail.com>
2023-01-16 03:03:30 +0900

    DEV-54 fix: 각각의 요청 endpoint 목적에 부합하게 변경

--- server/backend/src/domain/user/routes/userRoutes.js
+++ server/backend/src/domain/user/routes/userRoutes.js
@@ -4,27 +4,16 @@ import userControllers from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.route('/login').post(authControllers.userLogIn);
-
-router.route('/refresh').get(authControllers.refreshJWT);
-
-// 토큰정보를 기반으로 권한이 있는지 미들웨어에서 확인.
-router
-  .route('/signinOptions/forgotPassword')
-  .post(authControllers.authJWT, authControllers.sendResetEmail);
-
-router
-  .route('/signinOptions/resetPassword/:token')
-  .patch(authControllers.resetPassword);
-
-router.route('/checkPageToken/:token').get(authControllers.validateResetPage);
-
 router.route('/').get(authControllers.authJWT, userControllers.getAllUsers);
-
-router.route('/check-duplicate').post(userControllers.checkDuplicate);
 
 router.route('/').post(userControllers.signupUser);
 
+router.route('/password/:token').patch(authControllers.resetPassword);
+
 router.route('/:token').delete(userControllers.withdrawUser);
+
+router
+  .route('/signinOptions/forgotPassword')
+  .post(authControllers.authJWT, authControllers.sendResetEmail);
 
 export default router;
