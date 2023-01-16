aa3cc2a07ea93fff268dec50be44a6451b0cdf0b
suhwan2004 <suhwan2004@gmail.com>
2023-01-16 21:54:32 +0900

    feature : instance.js에서 token 관련 에러코드일 때의 분기처리를 최신 코드에 맞게 수정

--- Client/src/api/instance.js
+++ Client/src/api/instance.js
@@ -5,6 +5,13 @@ import axios from 'axios';
 
 let isTokenRefreshing = false;
 let refreshSubscribers = [];
+const tokenErrorCodeArr = ['E400AD', 'E401AB', 'E401AC', 'E404AC'];
+/*
+      E400AD: 'Bad Request: JsonWebToken is invaid',
+      E401AB: 'Not Authorized: AccessToken is expired',
+      E401AC: 'Not Authorized: RefreshToken is expired',
+      E404AC: 'Not Found: RefreshToken is invalid or does not exist',
+    */
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_MAIN_URL,
@@ -41,6 +48,7 @@ const getTokenRefresh = async () => {
 instance.interceptors.request.use(
   (config) => {
     const accessToken = sessionStorage.getItem('accessToken');
+    // accessToken이 존재한다면, header에 저장.
     if (config.headers && accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
@@ -53,8 +61,11 @@ instance.interceptors.response.use(
   (response) => response,
   async (error) => {
     const { config, response } = error;
-    // 만약, 401코드가 아니라면 토큰 재발급 관련 오류가 아님. 에러 처리를 해주도록 함.
-    if (response.status !== 401) return Promise.reject(error);
+
+    // 만약, 토큰 관련 에러가 아니라면 단순 에러이기 때문에 바로, 에러 처리를 해주도록 함.
+    if (!response || !tokenErrorCodeArr.includes(response.data.errorCode)) {
+      return Promise.reject(error);
+    }
 
     // token이 refresh 되는 중임. 지금 현재 요청을 구독목록에 저장하고, refresh가 풀린 이후에 실행 되도록 처리함.
     if (isTokenRefreshing) {
