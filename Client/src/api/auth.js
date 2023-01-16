630e0b3b706b2ae4c16efd44cc1950539c0be1bd
suhwan2004 <suhwan2004@gmail.com>
2023-01-16 20:48:40 +0900

    feature : api/auth.js 에서 postFindPw를 최신 코드에 맞게 수정처리

--- Client/src/api/auth.js
+++ Client/src/api/auth.js
@@ -10,25 +10,21 @@ const postUserSignIn = async (userEmail, userPassword) => {
 
     return res;
   } catch (e) {
-    return {
-      ...e.response,
-    };
+    return e;
   }
 };
 
 const postFindPw = async (email) => {
-  const API_END_POINT = `${process.env.REACT_APP_SERVER_URL}/api/v1/auth/findPw`;
+  const API_END_POINT = `${process.env.REACT_APP_SERVER_URL}/api/v1/user/signinOptions/forgotPassword`;
 
   try {
     const res = await axios.post(API_END_POINT, {
-      user_email: email,
+      userEmail: email,
     });
 
     return res;
   } catch (e) {
-    return {
-      ...e.response,
-    };
+    return e;
   }
 };
 
