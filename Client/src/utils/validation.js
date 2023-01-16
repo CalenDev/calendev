38ec6c96de80e3acc2f33f707d2726ac3604c1d3
suhwan2004 <suhwan2004@gmail.com>
2023-01-17 00:16:15 +0900

    feature : validation.js 삭제

--- Client/src/utils/validation.js
+++
@@ -1,7 +0,0 @@
-function validatePassword(password) {
-  const reg =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
-  return reg.test(password);
-}
-
-export default validatePassword;
