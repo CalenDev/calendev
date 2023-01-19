aa771de44a383cecbb211d1cb2169bc87b76fb03
ekgns33 <76658405+ekgns33@users.noreply.github.com>
2023-01-19 21:17:48 +0900

    Ekgns33/enhance/fix jwtservice and create user role cd enum (#34)
    * Enhance fix : jwt 로직 수정에 따른 유저서비스 코드 수정
    
    * Enhance feature: user role code enum
    
    * Enhance fix: authController.js의 authJWT : 액세스토큰 만료로 인한 에러코드 변경
    
    * Enhance fix: E400AD 에러코드 description 오탈자 수정
    
    * Enhance fix: post 태그, 카테고리 enum객체 value값 Symbol에서 String으로 변환
    
    * Enhance fix: jwt.js의 verifyRefreshToken의 로직수정;상황에 맞는 에러처리 및 레디스에 질의하는 데이터 문자열형식으로 변환
    
    * Enhance fix: refresh.js 반환하는 에러코드 변경 및 verifyRefreshToken이 throw하는 에러 처리 코드 추가
    
    * Enhanc fix: 레디스에 들어가는 데이터 유효시간 설정
    
    * Enhance fix: 변수명 변경, E500AE 에러코드 추가
    
    * Enhance fix: refresh.js 코드리뷰 수정 > 리프레시토큰 에러 처리과정에서 E404AC에러 조건문에 추가

--- server/backend/src/global/common/Enum/eventTag.js
+++ server/backend/src/global/common/Enum/eventTag.js
@@ -1,16 +1,16 @@
 const EventTag = {
-  AAA: Symbol('공개채용'),
-  AAB: Symbol('개발 챌린지'),
-  AAC: Symbol('개발자 매칭'),
-  AAD: Symbol('프로그래밍 경진대회'),
-  AAE: Symbol('부트캠프'),
-  AAF: Symbol('컨퍼런스'),
-  AAG: Symbol('해커톤'),
-  AAH: Symbol('세미나'),
-  AAI: Symbol('교육'),
-  AAJ: Symbol('자격증 시험'),
-  AAK: Symbol('스터디'),
-  AAL: Symbol('멘토링'),
+  AAA: '공개채용',
+  AAB: '개발 챌린지',
+  AAC: '개발자 매칭',
+  AAD: '프로그래밍 경진대회',
+  AAE: '부트캠프',
+  AAF: '컨퍼런스',
+  AAG: '해커톤',
+  AAH: '세미나',
+  AAI: '교육',
+  AAJ: '자격증 시험',
+  AAK: '스터디',
+  AAL: '멘토링',
 };
 Object.freeze(EventTag);
 
