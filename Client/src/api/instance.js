f7f091d1beb827f04d9e703188bb3488f5cc0470
suhwan2004 <suhwan2004@gmail.com>
2023-01-19 21:09:35 +0900

    feature : instance.js에서 switch case 부분의 코드 설명 주석 추가

--- Client/src/api/instance.js
+++ Client/src/api/instance.js
@@ -1,44 +1,10 @@
 /* eslint-disable no-param-reassign */
-/* eslint-disable no-use-before-define */
 
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { logoutUser } from '../features/User/UserSlice';
 import { openModal } from '../features/GlobalModal/GlobalModalSlice';
-import { useNavigate } from 'react-router-dom';
-
-
-/*
-    엑세스 토큰 재발급 : E400AA, E400AD, E401AB
-    로그아웃 : E400AB, E401AC, E404AC
-    */
-    
-/*
-{
-"userId": "48",
-"organizerId": "22",
-"postTitle": "김수환군은 문다훈군의 컴퓨터를 파괴하려했다.",
-"postContent": "안녕하세요또만나요잘지내요",
-"postThumbnailImg": [],
-"postImg": [],
-"postTag": 
-[
-  { 
-    "tagName": "A5",
-    "tagCategory": ["C1", "C3"]
-  },
-  { 
-    "tagName": "A9",
-    "tagCategory": ["C4", "C7"],
-  }
-],
-"postPlace": "충청남도 예산군 오가중앙로111 예산소방서",
-"postContactPhone": "0411232123",
-"eventStartDttm": "2022-10-17 15:50:14",
-"eventEndDttm": "2022-10-19 15:50:14"}
-*/
-
-const navigate = useNavigate();
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_MAIN_URL,
@@ -62,39 +28,40 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const { response } = error;
+    const { response, config } = error;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    // connection error or unnomal error
+    /*
+     - response가 없는 경우 : 아예, 서버로 가지도 못한 경우
+     - data가 없는 경우, errorCode가 없는 경우 : 서버에서 받게된 예기치 못한 response
+    */
     if (!response || !response.data || !response.data.errorCode) {
-      return Promise.reject(error);
+      return Promise.reject(error); // 에러를 그대로 반환
     }
-    
-    const errorCode = response.data.errorCode;
-    
-    Switch(errorCode){
-      case "E400AA":  //access Token Refreshing
-      case "E400AD":
-      case "E401AB":
+
+    const { errorCode } = response.data;
+    switch (errorCode) {
+      case 'E400AA':
+      case 'E400AD':
+      case 'E401AB':
         try {
-          const apiRes = await instance.get('/tokenRefresh');
+          const apiRes = await instance.get('/refresh'); // 엑세스 토큰 갱신
           sessionStorage.setItem('accessToken', apiRes.data.accessToken);
-          return Promise.resolve(apiRes);
+          return instance(config); // 기존 실패했던 request를 다시 보냄.
         } catch (e) {
-          sessionStorage.removeItem('accessToken');
+          sessionStorage.removeItem('accessToken'); //
           return Promise.reject(e);
         }
-        break;
-      case "E400AB": //logout
-      case "E401AC":
-      case "E404AC":
+      case 'E400AB':
+      case 'E401AC':
+      case 'E404AC':
         dispatch(logoutUser()); // redux 로그인 상태 초기화
-        dispatch(openModal({ modalCode : 3 })); //redux 로그인 유도 모달창 생성
-        navigate("/signin",{ replace : true }); // 로그인 페이지로 이동
-        return;
-        break;
-      default:
-        return Promise.reject(e);
-        break;
+        dispatch(openModal({ modalCode: 3 })); // redux 로그인 유도 모달창 생성
+        navigate('/signin', { replace: true }); // 로그인 페이지로 이동
+        return Promise.reject(error);
+      default: // 나머지 에러코드는 페이지에게 할당.
+        return Promise.reject(error);
     }
   },
 );
