80c41a316917a1453fe405297fd450543d9a7299
suhwan2004 <suhwan2004@gmail.com>
2023-01-21 05:59:42 +0900

    fix : instance.js에서 로그아웃 처리를 맡던 부분을 삭제

--- Client/src/api/instance.js
+++ Client/src/api/instance.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-param-reassign */
 
 import axios from 'axios';
-// import { useDispatch } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { logoutUser } from '../features/User/UserSlice';
-// import { openModal } from '../features/GlobalModal/GlobalModalSlice';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_MAIN_URL,
@@ -35,28 +31,22 @@ instance.interceptors.response.use(
      - response가 없는 경우 : 아예, 서버로 가지도 못한 경우
      - data가 없는 경우, errorCode가 없는 경우 : 서버에서 받게된 예기치 못한 response
     */
+
     if (!response || !response.data || !response.data.code) {
-      return Promise.reject(error); // 에러 페이지로 갈 거임. 렌더링은 이럴 때만 e.response.data.message로
+      return Promise.reject(error);
     }
 
-    const { code } = response.data;
+    const code = response.data.code || response.data.errorCode;
     switch (code) {
       case 'E400AA':
       case 'E401AB':
         try {
           const apiRes = await instance.get('/api/v1/auth/refresh'); // 엑세스 토큰 갱신
           sessionStorage.setItem('accessToken', apiRes.data.data.accessToken);
-          return instance({ url: config.url, method: config.method }); // 기존 실패했던 request를 다시 보냄.
+          return instance({ ...config, headers: { ...config.headers } }); // 기존 실패했던 request를 다시 보냄.
         } catch (e) {
-          // 실패했을때 로그아웃 시키고 로그인페이지로...
-          sessionStorage.removeItem('accessToken');
           return Promise.reject(e);
         }
-      case 'E400AB':
-      case 'E400AD':
-      case 'E401AC':
-      case 'E404AC':
-        return Promise.reject(error);
       default: // 나머지 에러코드는 페이지에게 할당.
         return Promise.reject(error);
     }
