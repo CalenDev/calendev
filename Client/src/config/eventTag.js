14ef809e2a8bb4428ce7d2856475918bc79e3025
dayeon-alert <dayeon.alert@gmail.com>
2023-01-25 18:00:15 +0900

    chore: add config files (DEV-68)

---
+++ Client/src/config/eventTag.js
@@ -0,0 +1,17 @@
+const EventTag = {
+  AAA: '공개채용',
+  AAB: '개발 챌린지',
+  AAC: '개발자 매칭',
+  AAD: '프로그래밍 경진대회',
+  AAE: '부트캠프',
+  AAF: '컨퍼런스',
+  AAG: '해커톤',
+  AAH: '세미나',
+  AAI: '교육',
+  AAJ: '자격증 시험',
+  AAK: '스터디',
+  AAL: '멘토링',
+};
+Object.freeze(EventTag);
+
+export default EventTag;
