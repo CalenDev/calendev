8a8381c4fbe2eda3c84f41ab623972d098ac6173
Dayeon <dayeon.alert@gmail.com>
2023-01-25 13:25:12 +0900

    feat: create CommonSelectBox component (DEV-66) (#37)

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,8 +1,9 @@
 import Header from './Header';
 import CommonPaper from './CommonPaper';
+import CommonSelectBox from './CommonSelectBox';
 import CommonStack from './CommonStack';
 import CommonTextField from './CommonTextField';
 
 export {
-  Header, CommonPaper, CommonStack, CommonTextField,
+  Header, CommonPaper, CommonStack, CommonTextField, CommonSelectBox
 };
