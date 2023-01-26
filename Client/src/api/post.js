a99d0fcecf4894535f005d21b12d028d15da081a
suhwan2004 <suhwan2004@gmail.com>
2023-01-26 23:48:23 +0900

    feature : api/post.js에 search request function 구현

--- Client/src/api/post.js
+++ Client/src/api/post.js
@@ -1,8 +1,43 @@
 import instance from './instance';
+import { validateSinceAndEnd } from '../utils/validationCheck';
+import changeDefaultTimeFormat from '../utils/change';
 
-const postSearchByOptions = async () => {
+const postSearchByOptions = async (props) => {
+  const { since, end, page, limit, sortBy, inputString, tags } = props;
+  const makeQueryString = () => {
+    const queryStringObj = {
+      page,
+      limit,
+    };
+
+    // 시작일과 끝일이 올바른가? => 올바르지 않을 경우 자동적으로 넣지 않음.
+    if (validateSinceAndEnd(since, end)) {
+      queryStringObj.since = changeDefaultTimeFormat(since);
+      queryStringObj.end = changeDefaultTimeFormat(end);
+    }
+
+    // 검색어가 입력되었는가?
+    if (inputString.length > 0) {
+      queryStringObj.inputString = inputString;
+    }
+
+    // 정렬 조건이 있는가?
+    if (sortBy.length > 0) {
+      queryStringObj.sortBy = sortBy;
+      if (sortBy === 'eventStartDttm') {
+        queryStringObj.since = changeDefaultTimeFormat(new Date());
+        delete queryStringObj.end;
+      }
+    }
+    return Object.entries(queryStringObj)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&');
+  };
+
   try {
-    const response = await instance.post('/search');
+    const response = await instance.post(`/search?${makeQueryString()}`, {
+      tags,
+    });
     return response;
   } catch (e) {
     return e.response;
