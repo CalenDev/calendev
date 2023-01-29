964db4d86284afcc6aa13dd7a7b551797ea8bd86
suhwan2004 <suhwan2004@gmail.com>
2023-01-29 21:03:21 +0900

    fix : Write Post page의 Route path 수정

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,9 +12,10 @@ import Home from './pages/Home/Home';
 import ResetPw from './pages/ResetPw/ResetPw';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
-import GlobalModal from './features/GlobalModal';
+import WritePost from './pages/WritePost/WritePost';
 import PostCalendar from './pages/Post/PostCalendar/PostCalendar';
 import PrivateRoute from './components/PrivateRoute/index';
+import GlobalModal from './features/GlobalModal/index';
 
 function App() {
   const theme = responsiveFontSizes(createTheme(themes));
@@ -32,6 +33,7 @@ function App() {
           <Route path="/signup" exact element={<SignUp />} />
           <Route path="/findpw" exact element={<FindPw />} />
           <Route path="/resetpw" exact element={<ResetPw />} />
+          <Route path="/writepost" exact element={<WritePost />} />
         </Route>
       </Routes>
     </ThemeProvider>
