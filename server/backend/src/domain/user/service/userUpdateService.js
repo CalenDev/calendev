45f7df798d727b19de1719fbd4a09f540e16c52b
ekgns33 <ekgns00421@gmail.com>
2023-02-04 22:31:43 +0900

    DEV-89 fix: userUpdateService.resetPassword의 param을 유저객체와 새로운 비밀번호로 수정

--- server/backend/src/domain/user/service/userUpdateService.js
+++ server/backend/src/domain/user/service/userUpdateService.js
@@ -3,23 +3,18 @@ import User from '../models/user.js';
 import encrypt from '../../../global/utils/encrypt.js';
 
 export default {
-  resetPassword: async (resetReq) => {
-    const users = await User.findTargetUserByEmail(resetReq.getUserEmail);
-    if (users.length === 0) {
-      throw new AppError('Not Found', 404, 'E404AD');
-    }
-
+  resetPassword: async (targetUser, changedPassword) => {
     const hashedPassword = await encrypt.createHashedPasswordBySalt(
-      resetReq.getUserPassword,
-      users[0].salt,
+      changedPassword,
+      targetUser.salt,
     );
 
-    if (hashedPassword === users[0].userPassword) {
+    if (hashedPassword === targetUser.userPassword) {
       throw new AppError('Not Authorized', 401, 'E401AD');
     }
 
     const userUpdateResult = await User.updateOne(
-      users[0].userId,
+      targetUser.userId,
       hashedPassword,
       'userPassword',
     );
