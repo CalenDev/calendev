53e7d899cb3a49c1d09acb9ae6c987ba965fa50d
ekgns33 <ekgns00421@gmail.com>
2023-02-05 00:56:05 +0900

    DEV-89 fix: userUpdateService.resetPassword 로직 수정

--- server/backend/src/domain/user/service/userUpdateService.js
+++ server/backend/src/domain/user/service/userUpdateService.js
@@ -1,20 +1,28 @@
 import AppError from '../../../global/utils/appError.js';
 import User from '../models/user.js';
 import encrypt from '../../../global/utils/encrypt.js';
+import redisConfig from '../../../global/config/redisConfig.js';
 
 export default {
-  resetPassword: async (targetUser, changedPassword) => {
+  resetPassword: async (user, changedPassword) => {
+    // 이전 비밀번호와 같으면 에러 리턴
     const hashedPassword = await encrypt.createHashedPasswordBySalt(
       changedPassword,
-      targetUser.salt,
+      user[0].salt,
     );
 
-    if (hashedPassword === targetUser.userPassword) {
+    if (hashedPassword === user[0].userPassword) {
       throw new AppError('Not Authorized', 401, 'E401AD');
     }
 
+    // 레디스에 refresh 토큰 있으면 삭제
+    redisConfig.redisClient.exists(user[0].userId, (err, ok) => {
+      if (err) throw err;
+      redisConfig.redisCli.del(`${user[0].userId}`);
+    });
+
     const userUpdateResult = await User.updateOne(
-      targetUser.userId,
+      user[0].userId,
       hashedPassword,
       'userPassword',
     );
